2169274cd47045c84d4406a3edb65304e3c06ae0
wal16 <waldemar.mozola@onet.pl>
2018-01-12 23:21:49 +0100

    hkjk

--- jdqz1-java-master/zadania/src/main/java/com/infoshareacademy/javaintroduction/task16/Schoolboy.java
+++ jdqz1-java-master/zadania/src/main/java/com/infoshareacademy/javaintroduction/task16/Schoolboy.java
@@ -6,29 +6,44 @@ package com.infoshareacademy.javaintroduction.task16;
  */
 public class Schoolboy {
 
-    public static int[] grade;
+    public  static int[] grade;
     private String average;
+    public String name;
+    public String surname;
+
+
 
 
     public static void main(String[] args) {
 
-        try
-        {
-            Schoolboy obj = new Schoolboy ();
-            obj.run (args);
+        Schoolboy[] schoolboys = new Schoolboy[3];
+        String[] name = {"Abraham", "Thomas", "Nikola"};
+        String[] surname = {"Lincoln", "Edison", "Tesla"};
+        for (int i = 0; i < schoolboys.length; i++) {
+            schoolboys[i] = new Schoolboy();
+            schoolboys[i].name = name[i];
+            schoolboys[i].surname = surname[i];
+            schoolboys[i].grade = new int[]{1, 2, 3, 4};
         }
-        catch (Exception e)
-        {
-            e.printStackTrace ();
+        System.out.println("Schoolboys are");
+        for (int i = 0; i < schoolboys.length; i++) {
+            System.out.println(schoolboys[i].name + " " + schoolboys[i].name + ", " + calcAverage(schoolboys[i].grade) + " grade");
+        }
+
+
+        try {
+            Schoolboy obj = new Schoolboy();
+            obj.run(args);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
 // instance variables here
 
-    public void run (String[] args) throws Exception
-    {
-        this.grade = new int[]{10, 11, 88, 2, 12, 120};
-    }
+    public void run(String[] args) throws Exception {
+        this.grade = new int[]{5, 4, 3 };
+
 
         // Calling getMax() method for getting max value
         int max = getMax(grade);
@@ -39,8 +54,11 @@ public class Schoolboy {
         System.out.println("Minimum Value is: " + min);
 
         Schoolboy thisObj = new Schoolboy();
-        thisObj.calcAverage();
+        thisObj.calcAverage(int[] grade);
+        System.out.println("Average Value is: " + thisObj.calcAverage(int[] grade));
 
+
+    }
 
     // Method for getting the maximum value
     public static int getMax(int[] inputArray) {
@@ -65,12 +83,13 @@ public class Schoolboy {
         return minValue;
     }
 
-    public static double calcAverage() {
+    public static double calcAverage(int[] grade) {
         double sum = 0;
         for (int i = 0; i < grade.length; i++) {
             sum = sum + grade[i];
         }
         double average = sum / (double) grade.length;
         return average;
+
     }
 }
--- jdqz1-java-master/zadania/src/main/java/com/infoshareacademy/javaintroduction/task17/Uczen.java
+++ jdqz1-java-master/zadania/src/main/java/com/infoshareacademy/javaintroduction/task17/Uczen.java
@@ -3,5 +3,74 @@ package com.infoshareacademy.javaintroduction.task17;
 /**
  * Created by waldemar on 2018-01-06.
  */
-public class Uczen {
-}
+
+    public class Uczen {
+        private String imie;
+        private String nazwisko;
+        private double[] oceny;
+
+
+        Uczen(String imie, String nazwisko, double[] oceny) {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.oceny = oceny;
+        }
+
+        public String getImie() {
+            return imie;
+        }
+
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public double[] getOceny() {
+            return oceny;
+        }
+
+        public void srednia() {
+            double suma=0;
+            for (double ocena : oceny){
+                suma += ocena;
+            }
+            double srednia = suma/oceny.length;
+            System.out.println("Średnia ucznia " + this.imie + " to: " + srednia);
+        }
+
+        public void maksymalna(){
+            if (oceny.length>0) {
+                double maksymalna = 0;
+                for (double ocena : oceny) {
+                    if (maksymalna<ocena) {
+                        maksymalna = ocena;
+                    }
+                }
+                System.out.println("Maksymalna ocena ucznia " + this.imie + " to: " + maksymalna);
+            }
+            else
+                System.out.println("Zero ocen");
+        }
+
+        public void minimalna(){
+            if (oceny.length>0) {
+                double minimalna = 0;
+                for (double ocena : oceny) {
+                    if (minimalna>ocena) {
+                        minimalna = ocena;
+                    }
+                }
+                System.out.println("Minimalna ocena ucznia " + this.imie + " to: " + minimalna);
+            }
+            else
+                System.out.println("Zero ocen");
+
+        }
+        public void wypiszOceny() {
+            for (double ocena : oceny) {
+                System.out.print(ocena + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
